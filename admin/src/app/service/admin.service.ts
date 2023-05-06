050c883bee5955ca509854a33c8d3d8390ce1eca
Dh4ki <edu_gv_14@hotmail.com>
2023-05-06 18:31:05 -0500

    INSTALACION DE GUARDS
    Se instala la protección de los componentes con GUARDS para el panel administrador

--- admin/src/app/service/admin.service.ts
+++ admin/src/app/service/admin.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from "rxjs";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { GLOBAL } from "./GLOBAL";
+import { JwtHelperService, JwtModule } from "@auth0/angular-jwt";
 
 @Injectable({
   providedIn: 'root'
@@ -23,6 +24,33 @@ export class AdminService {
 
   getToken(){
     return localStorage.getItem('token');
+  }
+
+  public isAuthenticated(allowRoles : string[]):boolean{
+
+    const token = localStorage.getItem('token');
+    
+    if (!token) {
+      return false;
+    }
+
+    try {
+      const helper = new JwtHelperService();
+      var decodedToken = helper.decodeToken(token|| "");
+      console.log(decodedToken);
+      if (!decodedToken) {
+        console.log('NO ACCESO');
+        localStorage.removeItem('token');
+        return false;
+    }
+    } catch (error) {
+      localStorage.removeItem('token');
+      return false;
+    }
+
+    
+
+    return allowRoles.includes(decodedToken['role']);
   }
 
 }
