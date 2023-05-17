28b85774a998a48d0dab0aed09ce7e6f973178df
Dh4ki <edu_gv_14@hotmail.com>
2023-05-17 18:13:15 -0500

    CONFIGURACIONES
    Se esta creando el modulo para configuraciones, hasta el momento se creo el metodo en el back y el front para el admin, faltan enlazar
    estos y que se cree en el back correspondientemente.

---
+++ admin/src/app/components/config/config.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { AdminService } from 'src/app/services/admin.service';
+import { v4 as uuidv4 } from 'uuid';
+
+declare var iziToast:any;
+
+@Component({
+  selector: 'app-config',
+  templateUrl: './config.component.html',
+  styleUrls: ['./config.component.css']
+})
+export class ConfigComponent implements OnInit{
+
+  public token;
+  public config:any=[];
+  public titulo_cat = '';
+  public icono_cat = '';
+
+  constructor(
+    private _adminService: AdminService
+  ){
+    this.token = localStorage.getItem('token');
+    this._adminService.obtener_config_admin(this.token).subscribe(
+      response=>{
+        this.config=response.data;
+        console.log(this.config);
+      },error=>{
+        console.log(error);
+      }
+    );
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  agregar_cat(){
+    if (this.titulo_cat && this.icono_cat) {
+      console.log(uuidv4());
+      this.config.categorias.push({
+        titulo: this.titulo_cat,
+        icono: this.icono_cat,
+        _id: uuidv4()
+      });
+      this.titulo_cat='';
+      this.icono_cat='';
+    }else{
+      iziToast.show({
+        title: 'ERROR',
+        titleColor: '#FFA500',
+        theme: 'dark',
+        class: 'text-danger',
+        position: 'topRight',
+        message: 'Debe ingresar un título para la categoría'
+      });
+    }
+  }
+}
