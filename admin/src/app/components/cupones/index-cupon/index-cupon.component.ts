6343c3e3309a495e9fce600ebfd2c90ec9eb5db3
Dh4ki <edu_gv_14@hotmail.com>
2023-05-17 13:02:27 -0500

    ELIMINAR CUPON
    Se creo el metodo eliminar cupon, se lista los cupones por fecha de creación.
    Se elimina en el back.

--- admin/src/app/components/cupones/index-cupon/index-cupon.component.ts
+++ admin/src/app/components/cupones/index-cupon/index-cupon.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { CuponService } from 'src/app/services/cupon.service';
 
+declare var iziToast:any;
+declare var JQuery:any;
+declare var $:any;
+
 @Component({
   selector: 'app-index-cupon',
   templateUrl: './index-cupon.component.html',
@@ -21,7 +25,7 @@ export class IndexCuponComponent implements OnInit{
     this.token = localStorage.getItem('token');
   }
 
-  ngOnInit(): void {
+  init_Data(){
     this._cuponService.listar_cupones_admin(this.filtro,this.token).subscribe(
       response=>{
         this.cupones=response.data;
@@ -30,11 +34,30 @@ export class IndexCuponComponent implements OnInit{
     )
   }
 
+  ngOnInit(): void {
+    this.init_Data();
+  }
+
   filtrar(){
-    this._cuponService.listar_cupones_admin(this.filtro,this.token).subscribe(
+    this.init_Data();
+  }
+
+  eliminar(id:any){
+    this._cuponService.eliminar_cupon_admin(id,this.token).subscribe(
       response=>{
-        this.cupones=response.data;
-        this.load_data=false;
+        iziToast.show({
+          title: 'SUCCESS',
+          titleColor: '#FFD700',
+          theme: 'dark',
+          class: 'text-success',
+          position: 'topRight',
+          message: 'Se eliminó correctamente el cliente.'
+        });
+        $('#delete-'+id).modal('hide');
+        $('.modal-backdrop').removeClass('show');
+        this.init_Data();
+      },error=>{
+        console.log(error);
       }
     )
   }
