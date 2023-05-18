23bea87658edc775829a243e9d585e1918aad578
Dh4ki <edu_gv_14@hotmail.com>
2023-05-17 19:22:37 -0500

    CONFGURACION DE TIENDA
    Se termino de configurar el modulo para las configuraciones de la tienda.
    Esto incluye: nombre de la tienda, facturación, categorías y logo.
    Todos los campos se actualizan correctamente con el back y tiene el mismo sistema que el producto apra la imagen.

--- admin/src/app/components/config/config.component.ts
+++ admin/src/app/components/config/config.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { GLOBAL } from 'src/app/services/GLOBAL';
 import { AdminService } from 'src/app/services/admin.service';
 import { v4 as uuidv4 } from 'uuid';
 
 declare var iziToast:any;
+declare var JQuery:any;
+declare var $:any;
 
 @Component({
   selector: 'app-config',
@@ -15,14 +18,19 @@ export class ConfigComponent implements OnInit{
   public config:any=[];
   public titulo_cat = '';
   public icono_cat = '';
+  public file:any=undefined;
+  public imgSelect: any | ArrayBuffer;
+  public url;
 
   constructor(
     private _adminService: AdminService
   ){
     this.token = localStorage.getItem('token');
+    this.url = GLOBAL.url;
     this._adminService.obtener_config_admin(this.token).subscribe(
       response=>{
         this.config=response.data;
+        this.imgSelect = this.url+'obtener_logo/'+this.config.logo;
         console.log(this.config);
       },error=>{
         console.log(error);
@@ -54,5 +62,96 @@ export class ConfigComponent implements OnInit{
         message: 'Debe ingresar un título para la categoría'
       });
     }
+  }
+
+  actualizar(confForm:any){
+    if (confForm.valid) {
+      let data={
+        titulo: confForm.value.titulo,
+        serie: confForm.value.serie,
+        correlativo: confForm.value.correlativo,
+        categorias: this.config.categorias,
+        logo: this.file
+      }
+      console.log(data);
+      this._adminService.actualizar_config_admin("646550597b183a82ef66b1e8",data,this.token).subscribe(
+        response=>{
+          iziToast.show({
+            title: 'SUCCESS',
+            titleColor: '#FFD700',
+            theme: 'dark',
+            class: 'text-success',
+            position: 'topRight',
+            message: 'Se actualizó correctamente la configuración.'
+          });
+        }
+      );
+    }else{
+      iziToast.show({
+        title: 'ERROR',
+        titleColor: '#FFA500',
+        theme: 'dark',
+        class: 'text-danger',
+        position: 'topRight',
+        message: 'Complete correctamente el formulario'
+      });
+    }
+  }
+
+  fileChangeEvent(event:any):void{
+    var file;
+    if (event.target.files && event.target.files[0]) {
+      file=<File>event.target.files[0];
+    }else{
+      iziToast.show({
+        title: 'ERROR',
+        titleColor: '#FFA500',
+        theme: 'dark',
+        class: 'text-danger',
+        position: 'topRight',
+        message: 'No hay una imagen para subir'
+      });
+      
+    }
+    if (file && file.size <= 4000000) {
+      if (file.type == 'image/png' || file.type == 'image/webp' || file.type == 'image/jpg' || file.type == 'image/gif' || file.type == 'image/jpeg') {
+        const reader = new FileReader();
+        reader.onload = e => this.imgSelect = reader.result;
+        $('.cs-file-drop-icon').addClass('cs-file-drop-preview img-thumbnail rounded');
+        $('.cs-file-drop-icon').removeClass('cs-file-drop-icon cxi-upload');
+        reader.readAsDataURL(file);
+        // $('#input-portada').text(file.name);
+        this.file = file;
+      }else{
+        iziToast.show({
+          title: 'ERROR',
+          titleColor: '#FFA500',
+          theme: 'dark',
+          class: 'text-danger',
+          position: 'topRight',
+          message: 'El archivo debe de ser una imagen'
+        });
+        $('#input-portada').text('Seleccionar imagen');
+        this.imgSelect='assets/img/01.jpg';
+        this.file=undefined;
+      }
+    }else{
+      iziToast.show({
+        title: 'ERROR',
+        titleColor: '#FFA500',
+        theme: 'dark',
+        class: 'text-danger',
+        position: 'topRight',
+        message: 'La imagen no puede superar los 4MB'
+      });
+      $('#input-portada').text('Seleccionar imagen');
+      this.imgSelect='assets/img/01.jpg';
+      this.file=undefined;
+    }
+    console.log(this.file);
+  }
+
+  ngDoCheck():void{
+    $('.cs-file-drop-preview').html("<img src="+this.imgSelect+">");
   }
 }
--- back/controllers/ConfigController.js
+++ back/controllers/ConfigController.js
@@ -1,4 +1,6 @@
 var Config = require('../models/config');
+var fs = require('fs');
+var path = require('path');
 
 const obtener_config_admin = async function(req,res){
     if (req.user) {
@@ -22,12 +24,13 @@ const actualizar_config_admin = async function(req,res){
 
             if (req.files) {
                 //SI HAY IMAGEN
+                console.log('si hay imagen');
                 var img_path = req.files.logo.path;
                 var name = img_path.split('\\');
                 var logo_name = name[2];
 
                 let reg = await Config.findByIdAndUpdate({_id:"646550597b183a82ef66b1e8"},{
-                    categorias: data.categorias,
+                    categorias: JSON.parse(data.categorias),
                     titulo: data.titulo,
                     serie: data.serie,
                     logo: logo_name,
@@ -42,6 +45,7 @@ const actualizar_config_admin = async function(req,res){
                 });
                 res.status(200).send({data:reg});
             }else{
+                console.log('no hay imagen');
                 let reg = await Config.findByIdAndUpdate({_id:"646550597b183a82ef66b1e8"},{
                     categorias: data.categorias,
                     titulo: data.titulo,
@@ -58,7 +62,22 @@ const actualizar_config_admin = async function(req,res){
     }
 }
 
+const obtener_logo =async function(req,res){
+    var img = req.params['img'];
+    console.log(img);
+    fs.stat('./uploads/configuraciones/'+img,function(err){
+        if (!err) {
+            let path_img = './uploads/configuraciones/'+img;
+            res.status(200).sendFile(path.resolve(path_img));
+        }else{
+            let path_img = './uploads/default.jpg';
+            res.status(200).sendFile(path.resolve(path_img));
+        }
+    });
+}
+
 module.exports = {
     actualizar_config_admin,
-    obtener_config_admin
+    obtener_config_admin,
+    obtener_logo
 }
