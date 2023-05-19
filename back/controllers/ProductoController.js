2544e1c3d78dbb85a496c920d3266619bf594757
Dh4ki <edu_gv_14@hotmail.com>
2023-05-19 00:31:39 -0500

    GALERIA PRODUCTO
    se creó el módulo de galeria para un producto, el el cuál se puede subir varias imágenes de un producto.
    Al igual se realizó su método de eliminar el cual elimina del back.

--- back/controllers/ProductoController.js
+++ back/controllers/ProductoController.js
@@ -205,7 +205,7 @@ const registro_inventario_producto_admin = async function(req,res){
     }
 }
 
-const actualizar_producto_variedades_admin=async function(req,res){
+const actualizar_producto_variedades_admin = async function(req,res){
     if (req.user) {
         if (req.user.role=='admin') {
             let id = req.params['id'];
@@ -224,7 +224,43 @@ const actualizar_producto_variedades_admin=async function(req,res){
     }
 }
 
-module.exports={
+const agregar_imagen_galeria_admin = async function(req,res){
+    if (req.user) {
+        if (req.user.role=='admin') {
+            let id = req.params['id'];
+            let data=req.body;
+            var img_path = req.files.imagen.path;
+            var name = img_path.split('\\');
+            var imagen_name = name[2];
+            let reg = await Producto.findByIdAndUpdate({_id:id},{ $push: {galeria:{
+                imagen: imagen_name,
+                _id: data._id
+            }}});
+            res.status(200).send({data:reg});
+        }else{
+            res.status(500).send({message: 'NoAccess'});
+        }
+    }else{
+        res.status(500).send({message: 'NoAccess'});
+    }
+}
+
+const eliminar_imagen_galeria_admin = async function(req,res){
+    if (req.user) {
+        if (req.user.role=='admin') {
+            let id = req.params['id'];
+            let data=req.body;
+            let reg = await Producto.findByIdAndUpdate({_id:id},{$pull:{galeria:{_id:data._id}}});
+            res.status(200).send({data:reg});
+        }else{
+            res.status(500).send({message: 'NoAccess'});
+        }
+    }else{
+        res.status(500).send({message: 'NoAccess'});
+    }
+}
+
+module.exports = {
     registro_producto_admin,
     listar_productos_admin,
     obtener_portada,
@@ -234,5 +270,7 @@ module.exports={
     listar_inventario_producto_admin,
     eliminar_inventario_producto_admin,
     registro_inventario_producto_admin,
-    actualizar_producto_variedades_admin
+    actualizar_producto_variedades_admin,
+    agregar_imagen_galeria_admin,
+    eliminar_imagen_galeria_admin
 }
