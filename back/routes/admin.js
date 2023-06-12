487abe42a78506e0399016379415552b76b912b0
Dh4ki <edu_gv_14@hotmail.com>
2023-06-12 08:13:37 -0500

    GESTIONAR MENSAJES EN ADMINISTRADOR
    Se creo la vista de los mensajes del administrador para responer las
    preguntas. Se gestiono los mensajes con dos tipos de estados en el panel
    del administrador.

--- back/routes/admin.js
+++ back/routes/admin.js
@@ -4,9 +4,13 @@ var express = require('express');
 var adminController = require('../controllers/AdminController');
 
 var api = express.Router();
+var auth = require('../middlewares/authenticate');
 
 api.post('/registro_admin',adminController.registro_admin);
 api.post('/login_admin',adminController.login_admin);
+
+api.get('/obtener_mensajes_admin',auth.auth,adminController.obtener_mensajes_admin);
+api.put('/cerrar_mensaje_admin/:id',auth.auth,adminController.cerrar_mensaje_admin);
 
 
 module.exports = api;
